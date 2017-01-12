640377b4d6a279deab2a39f37377a184fb0b1f6f
Valerio Proietti <kamicane@gmail.com>
2017-01-12 15:16:22 +0100

    Promise.resolve() is now called regardless, because it's safer against packages that provide a then function, but implement the method in a wrong manner. Got rid of the util script.

--- sequence.js
+++ sequence.js
@@ -1,33 +1,35 @@
-// inspired by co.js (https://github.com/tj/co)
-// can yield any value, not just promises
+// Works like async / await.
+// It is very similar to babel's async / await implementation.
+// I like to think this is faster because it doesn't do any useless try / catching
 'use strict'
 
-const util = require('./util')
-
-function create (gen) {
-  return new Promise((resolve, reject) => {
-    const genNext = function (value) {
-      next(gen.next(value))
-    }
-
-    const genThrow = function (error) {
-      next(gen.throw(error))
-    }
-
-    const next = function (result) {
-      let value = result.value
-      if (result.done) resolve(value)
-      else if (util.isThenable(value)) value.then(genNext, genThrow).catch(reject)
-      else genNext(value)
-    }
-
-    genNext()
-  })
-}
-
 module.exports = function sequence (gen) {
-  if (!util.isGeneratorFunction(gen)) throw new Error('sequence expects a generator function')
   return function () {
-    return create(gen.apply(this, arguments))
+    let it = gen.apply(this, arguments)
+
+    return new Promise((resolve, reject) => {
+      const genNext = function (value) {
+        next(it.next(value))
+      }
+
+      const genThrow = function (error) {
+        next(it.throw(error))
+      }
+
+      const next = function (result) {
+        let value = result.value
+        if (result.done) resolve(value)
+        // This calls Promise.resolve(value) because value might
+        // or might not be a promise, or even not a valid "thenable".
+        // Unless we get a native isPromise, this extra potentially
+        // useless call needs to be here.
+        else Promise.resolve(value).then(genNext, genThrow).catch(reject)
+        // Since errors are now caught by the new promise created
+        // with Promise.resolve(), we need to manually reject the
+        // original promise on error by doing a .catch().
+      }
+
+      genNext() // This is caught because it's inside new Promise
+    })
   }
 }
